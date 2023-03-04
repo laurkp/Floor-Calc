51c6f8b8348b46535ba172f9c5a131d1fbecbc56
laurkp <120257523+laurkp@users.noreply.github.com>
2023-03-04 20:12:42 +0000

    Update Program.cs
    #2 Correcting spelling error
    #3 
    #4 Using string interpolation instead of concatenation
    #5 Using floating point values (double) instead of integers
    #6 Using existing variables instead of hardcoded values

--- Floor Calc/Program.cs
+++ Floor Calc/Program.cs
@@ -9,71 +9,71 @@
         {
             // Asking for floor and tile dimensions and price per unit
             Console.WriteLine("Insert floor width: ");
-            int floorWidth = int.Parse(Console.ReadLine());
+            double floorWidth = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Insert floor lenght: ");
-            int floorLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert floor length: ");
+            double floorLength = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Insert tile width: ");
-            int tileWidth = int.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Insert tile lenght: ");
-            int tileLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert tile length: ");
+            double tileLength = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Insert price per unit of flooring: ");
-            int pricePerUnit = int.Parse(Console.ReadLine());
+            double pricePerUnit = double.Parse(Console.ReadLine());
 
             // Calculating floor area
-            int floorArea = floorWidth * floorLength;
+            double floorArea = floorWidth * floorLength;
 
-            Console.WriteLine("Floor area is: " + floorArea);
+            Console.WriteLine($"Floor area is: {floorArea}");
 
             //Calculating tile area
-            int tileArea = tileWidth * tileLength;
+            double tileArea = tileWidth * tileLength;
 
-            Console.WriteLine("Tile area is: " + tileArea);
+            Console.WriteLine($"Tile area is: {tileArea}");
 
             // Calculating the total price 
-            int total = floorArea / tileArea * pricePerUnit;
+            double total = floorArea / tileArea * pricePerUnit;
 
-            Console.WriteLine("Total price is: $" + total);
+            Console.WriteLine($"Total price is: ${total}");
 
-            Console.WriteLine("Calculate the labor cost if the average flooring team can only put in 20 square feet of flooring per hour at a cost of $86.00/hr");
+            Console.WriteLine($"Calculate the labor cost if the average flooring team can only put in {FLOOR_TEAM} square feet of flooring per hour at a cost of {HOUR_PRICE}/hr");
 
             //Calculating hours needed for the rooms 
-            int hoursN = floorArea / FLOOR_TEAM;
+            double hoursN = floorArea / FLOOR_TEAM;
 
-            Console.WriteLine("Total hours needed is: " + hoursN);
+            Console.WriteLine($"Total hours needed is: {hoursN}");
 
             //Calculating total labor cost
-            int laborC = hoursN * HOUR_PRICE;
+            double laborC = hoursN * HOUR_PRICE;
 
-            Console.WriteLine("Total labor cost is: $" + laborC);
+            Console.WriteLine($"Total labor cost is: ${laborC}");
 
             //Asking the floor dimensions for a non-rectangular room
             Console.WriteLine("Calculate how much flooring will be needed for a square room");
             Console.WriteLine("Insert floor side: ");
-            int floorSide = int.Parse(Console.ReadLine());
+            double floorSide = double.Parse(Console.ReadLine());
 
             // Calculating floor area for the square room
-            int squareFloorArea = floorSide * floorSide;
+            double squareFloorArea = floorSide * floorSide;
 
-            Console.WriteLine("Floor area for the square room is: " + squareFloorArea);
+            Console.WriteLine($"Floor area for the square room is: {squareFloorArea}");
 
             //Calculating how much flooring we need for the room
-            int flooringNeeded = squareFloorArea / tileArea;
+            double flooringNeeded = squareFloorArea / tileArea;
 
-            Console.WriteLine("Flooring needed for the square room: " + flooringNeeded);
+            Console.WriteLine($"Flooring needed for the square room: {flooringNeeded}");
 
             //Calculating hours needed for the square room
-            int squareHoursN = squareFloorArea / FLOOR_TEAM;
+            double squareHoursN = squareFloorArea / FLOOR_TEAM;
 
-            Console.WriteLine("Total hours needed for the square room is: " + squareHoursN);
+            Console.WriteLine($"Total hours needed for the square room is: {squareHoursN}");
 
             //Calculating total laborr cost for the square room
-            int squareLaborC = squareHoursN * HOUR_PRICE;
+            double squareLaborC = squareHoursN * HOUR_PRICE;
 
-            Console.WriteLine("Total labor cost for the square room is: $" + squareLaborC);
+            Console.WriteLine($"Total labor cost for the square room is: ${squareLaborC}");
         }
     }
 }
