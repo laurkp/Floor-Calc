e035c8943d7503e3c4c48d1a2f776ae67ee8909e
laurkp <120257523+laurkp@users.noreply.github.com>
2023-03-11 21:28:24 +0000

    Changing the user input with a list
    #10 extracting the common code from within the conditions

--- Floor Calc/Program.cs
+++ Floor Calc/Program.cs
@@ -8,8 +8,14 @@
         static void Main(string[] args)
         {
             // Asking the floor shape
-            Console.WriteLine("Choose floor shape (rectangle or circle): ");
-            string floorShape = Console.ReadLine();
+            Console.WriteLine("Choose floor shape\n1 - rectangle\n2 - circle ");
+            int floorShape = Convert.ToInt32(Console.ReadLine());
+
+            double floorArea = 0;
+            double flooringNeeded = 0;
+            double total = 0;
+            double hoursN = 0;
+            double laborC = 0;
 
             // Asking for tile dimensions and price
             Console.WriteLine("Insert tile width: ");
@@ -27,7 +33,7 @@
             double tileArea = tileWidth * tileLength;
             Console.WriteLine($"Tile area is: {tileArea}");
 
-            if (floorShape == "rectangle")
+            if (floorShape == 1)
             {
                 // Asking dimensions of the rectangle floor
                 Console.WriteLine("Insert floor width: ");
@@ -37,52 +43,35 @@
                 double floorLength = double.Parse(Console.ReadLine());
 
                 // Calculating floor area for the rectangle room
-                double floorArea = floorWidth * floorLength;
-
-                //Calculating how much flooring we need for the rectangle room
-                double flooringNeeded = floorArea / tileArea;
-
-                // Calculating the total price for the rectangle room
-                double total = floorArea / tileArea * pricePerUnit;
-
-                //Calculating hours needed for the rectangle room 
-                double hoursN = floorArea / FLOOR_TEAM;
-
-                //Calculating total labor cost for the rectangle room
-                double laborC = hoursN * HOUR_PRICE;
-
-                Console.WriteLine($"Floor area is: {floorArea}");
-                Console.WriteLine($"Flooring needed for the room: {flooringNeeded}");
-                Console.WriteLine($"Total price is: ${total}");
-                Console.WriteLine($"Total hours needed is: {hoursN}");
-                Console.WriteLine($"Total labor cost is: ${laborC}");
+                double rectangleArea = floorWidth * floorLength;
+                floorArea = rectangleArea;
             }
-            if (floorShape == "circle")
+            if (floorShape == 2)
             {
                 // Asking for the radius of the circle shaped room
                 Console.WriteLine("Insert the radius of a circle shaped floor: ");
                 double floorRadius = double.Parse(Console.ReadLine());
 
                 // Calculate the floor area for the circle shaped room
-                double circleArea = Math.PI * (floorRadius * floorRadius);
+                floorArea = Math.PI * (floorRadius * floorRadius);
+            }
+            //Calculating how much flooring we need
+            flooringNeeded = floorArea / tileArea;
 
-                //Calculating how much flooring we need for the circle shaped room
-                double circleFlooring = circleArea / tileArea;
+            // Calculating the total price 
+            total = floorArea / tileArea * pricePerUnit;
 
-                // Calculating the total price for the circle shaped room
-                double circleTotal = circleArea / tileArea * pricePerUnit;
+            //Calculating hours needed 
+            hoursN = floorArea / FLOOR_TEAM;
 
-                //Calculating hours needed for the circle shaped room 
-                double circleHoursN = circleArea / FLOOR_TEAM;
+            //Calculating total labor cost 
+            laborC = hoursN * HOUR_PRICE;
 
-                //Calculating total labor cost for circle shaped room
-                double circleLaborC = circleHoursN * HOUR_PRICE;
-
-                Console.WriteLine($"Floor area is: {circleArea}");
-                Console.WriteLine($"Flooring needed for the room: {circleFlooring}");
-                Console.WriteLine($"Total price is: ${circleTotal}");
-                Console.WriteLine($"Total hours needed is: {circleHoursN}");
-                Console.WriteLine($"Total labor cost is: ${circleLaborC}");
+            Console.WriteLine($"Floor area is: {floorArea}");
+            Console.WriteLine($"Flooring needed for the room: {flooringNeeded}");
+            Console.WriteLine($"Total price is: ${total}");
+            Console.WriteLine($"Total hours needed is: {hoursN}");
+            Console.WriteLine($"Total labor cost is: ${laborC}");
             }
         }
     }
